bcfd68436a906047f37d8e99a6bac4818ec0d55e
PengJiaJun <2779824672@qq.com>
2025-10-13 23:31:43 +0800

    local init

---
+++ firstSeason/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace firstSeason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWord!");
+            Console.WriteLine("C#学习第一天");
+            Console.WriteLine("Visual Studio 快捷键和IDEA还是差很多");
+            Console.WriteLine("为什么TAB后这个方法只出来半个括号( , 为什么 Main 后没有出来提示直接生成Main方法");
+            Console.WriteLine("namespace 是项目名称吗, 为什么文件初始化出来后只有一个 Console.WriteLine(\"HelloWorld!\")");
+            Console.WriteLine("吐槽 Visual Studio 各类选项卡, 代码提示, 感觉都好小");
+        }
+    }
+}
