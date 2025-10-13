5d38cc70fdad9c1eddc049967b0ee1dff389a455
PengJiaJun <2779824672@qq.com>
2025-10-13 23:52:24 +0800

    找到了commit-2

--- firstSeason/Program.cs
+++ firstSeason/Program.cs
@@ -13,6 +13,8 @@ namespace firstSeason
             Console.WriteLine("namespace 是项目名称吗, 为什么文件初始化出来后只有一个 Console.WriteLine(\"HelloWorld!\")");
             Console.WriteLine("吐槽 Visual Studio 各类选项卡, 代码提示, 感觉都好小");
             Console.WriteLine("git commit 到底在哪里啊, IDEA是有设置修改的代码默认都是暂存的(省去了git add .这一步), 找不到 commit 啊, 只有推送, 拉取");
+            Console.WriteLine("找到了 [提交临时数据] 就是的. Ctrl + Entenr");
+            Console.WriteLine("每次修改文件后记得 点那个 + 也就是 git add , 不然commit时是上次+时的文件");
         }
     }
 }
